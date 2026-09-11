7c5112e443cebb119a6303113b2e1b9ad4c6b0b4
KRH <Kiararh92@gmail.com>
2026-09-11 18:51:04 -0400

    test builds

---
+++ herbal_app/src/test/java/herbalapp/herbappTest.java
@@ -0,0 +1,25 @@
+package herbalapp;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class herbappTest {
+
+    @Test
+    void ranHerb() {
+        herbapp app = new herbapp();
+        String html = app.ranHerb();
+
+        String extracted = html.substring(html.indexOf("<p>") + 3, html.indexOf("</p"));
+        assertTrue(app.getAllHerbs().contains(extracted),
+                "Random herb should be one of the herbs in the list");
+    }
+
+    @Test
+    void testAllHerbs() {
+        herbapp app  = new herbapp();
+        assertEquals(5, app.getAllHerbs().size(),
+                "Herb List should contain 5 items.");
+    }
+}
